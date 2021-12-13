a78134c4f1b9d4de717676b3c42d64fd93e6ab47
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-13 10:41:12 +0530

    Update repo.py

--- RemiRobot/modules/repo.py
+++ RemiRobot/modules/repo.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 from RemiRobot.conf import get_str_key
-from RemiRobot import tbot
+
 
 REPO_TEXT = "**A Cute [BOT](https://telegra.ph/file/d2815963abcae6fa20dc8.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Owner ⇀ : 『 [Lovely](t.me/Horimaya) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @CrowdXStrike «««"
   
@@ -22,7 +22,7 @@ BUTTONS = InlineKeyboardMarkup(
   
   
 @pbot.on_message(filters.command(["repo"]))
-async def repo(tbot, update):
+async def repo(update):
     await update.reply_text(
         text=REPO_TEXT,
         reply_markup=BUTTONS,
